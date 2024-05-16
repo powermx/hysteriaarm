9a016c4d57f95582eef2490440e8a01967fdd4ba
Roberto Jiménez <powermx@gmail.com>
2024-05-15 23:47:21 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ read -p " ingresa tu dominio: " domain
 
 apt update -y; apt upgrade -y; apt install git -y
 
-git clone https://github.com/gatesccn01/UDPMOD.git
+git clone https://github.com/powermx/hysteriaarm.git
 
 dir=$(pwd)
 
@@ -19,28 +19,28 @@ sys=$(which sysctl)
 ip4t=$(which iptables)
 ip6t=$(which ip6tables)
 
-openssl genrsa -out ${dir}/UDPMOD/udpmod.ca.key 2048
-openssl req -new -x509 -days 3650 -key ${dir}/UDPMOD/udpmod.ca.key -subj "/C=CN/ST=GD/L=SZ/O=Udpmod, Inc./CN=Udpmod Root CA" -out ${dir}/UDPMOD/udpmod.ca.crt
-openssl req -newkey rsa:2048 -nodes -keyout ${dir}/UDPMOD/udpmod.server.key -subj "/C=CN/ST=GD/L=SZ/O=Udpmod, Inc./CN=${domain}" -out ${dir}/UDPMOD/udpmod.server.csr
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:${domain},DNS:${domain}") -days 3650 -in ${dir}/UDPMOD/udpmod.server.csr -CA ${dir}/UDPMOD/udpmod.ca.crt -CAkey ${dir}/UDPMOD/udpmod.ca.key -CAcreateserial -out ${dir}/UDPMOD/udpmod.server.crt
+openssl genrsa -out ${dir}/hysteriaarm/udpmod.ca.key 2048
+openssl req -new -x509 -days 3650 -key ${dir}/hysteriaarm/udpmod.ca.key -subj "/C=CN/ST=GD/L=SZ/O=Udpmod, Inc./CN=Udpmod Root CA" -out ${dir}/hysteriaarm/udpmod.ca.crt
+openssl req -newkey rsa:2048 -nodes -keyout ${dir}/hysteriaarm/udpmod.server.key -subj "/C=CN/ST=GD/L=SZ/O=Udpmod, Inc./CN=${domain}" -out ${dir}/hysteriaarm/udpmod.server.csr
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:${domain},DNS:${domain}") -days 3650 -in ${dir}/hysteriaarm/udpmod.server.csr -CA ${dir}/hysteriaarm/udpmod.ca.crt -CAkey ${dir}/hysteriaarm/udpmod.ca.key -CAcreateserial -out ${dir}/hysteriaarm/udpmod.server.crt
 
-sed -i "s/setobfs/${OBFS}/" ${dir}/UDPMOD/config.json
-sed -i "s#instDir#${dir}#g" ${dir}/UDPMOD/config.json
-sed -i "s#instDir#${dir}#g" ${dir}/UDPMOD/udpmod.service
-sed -i "s#iptb#${interfas}#g" ${dir}/UDPMOD/udpmod.service
-sed -i "s#sysb#${sys}#g" ${dir}/UDPMOD/udpmod.service
-sed -i "s#ip4tbin#${ip4t}#g" ${dir}/UDPMOD/udpmod.service
-sed -i "s#ip6tbin#${ip6t}#g" ${dir}/UDPMOD/udpmod.service
+sed -i "s/setobfs/${OBFS}/" ${dir}/hysteriaarm/config.json
+sed -i "s#instDir#${dir}#g" ${dir}/hysteriaarm/config.json
+sed -i "s#instDir#${dir}#g" ${dir}/hysteriaarm/hysteriaarm.service
+sed -i "s#iptb#${interfas}#g" ${dir}/hysteriaarm/hysteriaarm.service
+sed -i "s#sysb#${sys}#g" ${dir}/hysteriaarm/hysteriaarm.service
+sed -i "s#ip4tbin#${ip4t}#g" ${dir}/hysteriaarm/hysteriaarm.service
+sed -i "s#ip6tbin#${ip6t}#g" ${dir}/hysteriaarm/hysteriaarm.service
 
-chmod +x ${dir}/UDPMOD/*
+chmod +x ${dir}/hysteriaarm/*
 
-install -Dm644 ${dir}/UDPMOD/udpmod.service /etc/systemd/system
+install -Dm644 ${dir}/hysteriaarm/udpmod.service /etc/systemd/system
 
 systemctl daemon-reload
 systemctl start udpmod
 systemctl enable udpmod
 
-echo " obfs: ${OBFS}" > ${dir}/UDPMOD/data
-echo "port: 36712" >> ${dir}/UDPMOD/data
-echo "rago de puertos: 10000:65000" >> ${dir}/UDPMOD/data
-cat ${dir}/UDPMOD/data
+echo " obfs: ${OBFS}" > ${dir}/hysteriaarm/data
+echo "port: 36712" >> ${dir}/hysteriaarm/data
+echo "rago de puertos: 10000:65000" >> ${dir}/hysteriaarm/data
+cat ${dir}/hysteriaarm/data
